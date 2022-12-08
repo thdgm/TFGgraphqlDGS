1a4aeee1044c23ec91c2edb9f884786c089bc6ce
Diego García <767870@unizar.es>
2022-12-08 13:48:45 +0100

    Entities y repositorios primera version

---
+++ graphqlDGS_copia/src/main/resources/data.sql
@@ -0,0 +1,18 @@
+INSERT INTO "catalog" ("id", "title") VALUES ('catalog1','catalogTit1');
+INSERT INTO "catalog" ("id", "title") VALUES ('catalog2','catalogTit2');
+INSERT INTO "catalog" ("id", "title") VALUES ('catalog3','catalogTit3');
+
+INSERT INTO "catalogrecord" ("id", "title") VALUES ('cR1','cRTit1');
+INSERT INTO "catalogrecord" ("id", "title") VALUES ('cR2','cRTit2');
+INSERT INTO "catalogrecord" ("id", "title") VALUES ('cR3','cRTit3');
+INSERT INTO "catalogrecord" ("id", "title") VALUES ('cR4','cRTit4');
+
+INSERT INTO "dataservice" ("id", "title") VALUES ('dSer1','dServTit1');
+
+INSERT INTO "dataset" ("id", "title") VALUES ('d1','dTit1');
+
+INSERT INTO "datasetseries" ("id", "title") VALUES ('dS1','dSTit1');
+
+INSERT INTO "distribution" ("id", "title") VALUES ('Dist1','DistTit1');
+INSERT INTO "distribution" ("id", "title") VALUES ('Dis2','DistTit2');
+INSERT INTO "distribution" ("id", "title") VALUES ('Dist3','DistTit3');
