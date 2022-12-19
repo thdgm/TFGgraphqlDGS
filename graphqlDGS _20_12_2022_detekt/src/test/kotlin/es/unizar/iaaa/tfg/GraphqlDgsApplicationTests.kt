dc4cab8c4690920487027fbf4d65813537979dc7
Diego García <767870@unizar.es>
2022-12-19 23:29:58 +0100

    Queries y detekt

---
+++ graphqlDGS _20_12_2022_detekt/src/test/kotlin/es/unizar/iaaa/tfg/GraphqlDgsApplicationTests.kt
@@ -0,0 +1,13 @@
+package es.unizar.iaaa.tfg
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GraphqlDgsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+        // Test needed
+    }
+}
