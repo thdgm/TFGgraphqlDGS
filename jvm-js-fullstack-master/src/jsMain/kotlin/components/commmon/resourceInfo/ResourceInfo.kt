11d28955df69fa42b84f37d3f12a1a18879a65fe
Diego <dgamuro9@gmail.com>
2023-03-17 00:37:51 +0100

    jvm-js-fullstack-master, aplicación montada sobre build gradle multiplatform, construyendo el frontend v5

--- jvm-js-fullstack-master/src/jsMain/kotlin/components/commmon/resourceInfo/ResourceInfo.kt
+++ jvm-js-fullstack-master/src/jsMain/kotlin/components/commmon/resourceInfo/ResourceInfo.kt
@@ -8,20 +8,32 @@ import csstype.Display
 import csstype.Position
 import csstype.pct
 import csstype.rgba
+import mui.icons.material.ReplyOutlined
 import mui.material.Box
 import mui.material.Card
+import mui.material.CardActions
 import mui.material.CardContent
+import mui.material.IconButton
 import mui.material.ListItemButton
 import mui.material.Paper
+import mui.material.SpeedDialIcon
 import mui.material.Typography
 import mui.material.styles.TypographyVariant
 import mui.system.sx
 import react.FC
 import react.Props
+import react.router.useNavigate
 import react.useEffect
 
 
+
 val resourceInfo = FC<Props> {
+
+    val navigate = useNavigate()
+
+    fun handleClick(){
+        navigate("/")
+    }
 
     Paper {
         sx{
@@ -36,12 +48,26 @@ val resourceInfo = FC<Props> {
             backgroundColor = rgba(249,249,249,0.87)
             position = Position.relative
         }
+
         Card {
             sx{
                 width = Sizes.CardList.Width
                 display= Display.flex
             }
             Box {
+                CardActions{
+                    IconButton{
+                        sx{
+                            fontSize = 250.pct
+                        }
+                        onClick ={handleClick()}
+                        ReplyOutlined{
+                            sx{
+                                fontSize = 100.pct
+                            }
+                        }
+                    }
+                }
                 CardContent{
                     Typography{
                         variant = TypographyVariant.h6
