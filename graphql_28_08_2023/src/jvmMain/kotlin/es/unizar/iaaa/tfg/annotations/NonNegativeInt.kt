b4bdb77eb59d80b5286ba56a5cd1947efb466115
Diego <dgamuro9@gmail.com>
2023-09-09 10:36:20 +0200

    Cmabios en consultas.

--- graphql_28_08_2023/src/jvmMain/kotlin/es/unizar/iaaa/tfg/annotations/NonNegativeInt.kt
+++
@@ -1,6 +0,0 @@
-package es.unizar.iaaa.tfg.annotations
-
-/*
- * Data class. Implementation of type LangString.
- */
-data class NonNegativeInt(val value: UInt)
--- graphql_28_08_2023/src/jvmMain/kotlin/es/unizar/iaaa/tfg/jsonDataModels/ModelJsonMapping.kt
+++
@@ -1,6 +0,0 @@
-package es.unizar.iaaa.tfg.jsonDataModels
-
-/*
- *  ModelJsonMapping. Helps to group these classes.
- */
-interface ModelJsonMapping
